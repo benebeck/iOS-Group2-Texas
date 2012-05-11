21fec39ac96cd4091f6a90b410393eab4169ab7f
myappisbiggerthenyours <ErayForPresident@hotmail.com>
2012-05-11 06:44:25 +0200

    HI

---
+++ nopainbutshame/nopainbutshame/Firstview.h
@@ -0,0 +1,17 @@
+//
+//  Firstview.h
+//  nopainbutshame
+//
+//  Created by Riza Koray Alpdogan on 11.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class Secondview;
+
+@interface Firstview : UIViewController{
+    IBOutlet Secondview *secondview;    
+
+}
+
+@end
---
+++ nopainbutshame/nopainbutshame/Secondview.h
@@ -0,0 +1,13 @@
+//
+//  Secondview.h
+//  nopainbutshame
+//
+//  Created by Riza Koray Alpdogan on 11.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Secondview : UIViewController
+
+@end
