d00da63eacd3761bf38f914abfe14fb8174a3f02
Benedikt Beckmann <benedikt.beckmann@embl.de>
2012-05-19 10:40:54 +0200

    PlayerBene added

---
+++ nopainbutshame/PlayerBene.h
@@ -0,0 +1,13 @@
+//
+//  PlayerBene.h
+//  nopainbutshame
+//
+//  Created by Benedikt Beckmann on 19.05.12.
+//  Copyright (c) 2012 BioApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayerBene : NSObject
+
+@end
---
+++ nopainbutshame/PlayerBene.m
@@ -0,0 +1,13 @@
+//
+//  PlayerBene.m
+//  nopainbutshame
+//
+//  Created by Benedikt Beckmann on 19.05.12.
+//  Copyright (c) 2012 BioApps. All rights reserved.
+//
+
+#import "PlayerBene.h"
+
+@implementation PlayerBene
+
+@end
